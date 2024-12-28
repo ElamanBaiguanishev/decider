ddee846800530c3ee5271f2168069c517c437b75
ElamanBaiguanishev <elaman1007@mail.ru>
2024-12-28 11:51:11 +0500

    Добавил моковый вход

--- frontend/src/components/common/topbar/Topbar.tsx
+++ frontend/src/components/common/topbar/Topbar.tsx
@@ -1,15 +1,24 @@
-import { AppBar, Toolbar, Typography, IconButton, Drawer } from "@mui/material";
+import { AppBar, Toolbar, Typography, IconButton, Drawer, Button } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
+import LogoutIcon from "@mui/icons-material/Logout";
 import { FC, useState } from "react";
 import MobileNav from "./MobileNav";
 import DesktopNav from "./DesktopNav";
-import './header.css'
+import './header.css';
+import { useAppSelector, useAppDispatch } from "../../../store/hooks";
+import { clearUser } from "../../../store/user/userSlice";
 
 const Topbar: FC = () => {
   const [mobileOpen, setMobileOpen] = useState(false);
+  const user = useAppSelector((state) => state.user.user);
+  const dispatch = useAppDispatch();
 
   const handleDrawerToggle = () => {
     setMobileOpen(!mobileOpen);
+  };
+
+  const handleLogout = () => {
+    dispatch(clearUser());
   };
 
   return (
@@ -18,6 +27,27 @@ const Topbar: FC = () => {
         <Typography variant="h6" component="div">
           MyApp
         </Typography>
+
+        <div style={{ display: "flex", alignItems: "center", gap: "10px" }}>
+          {user ? (
+            <>
+              <Typography variant="body1" component="span">
+                {user.name} {user.id}
+              </Typography>
+              <Button
+                color="inherit"
+                startIcon={<LogoutIcon />}
+                onClick={handleLogout}
+              >
+                Выйти
+              </Button>
+            </>
+          ) : (
+            <Typography variant="body1" component="span">
+              Гость
+            </Typography>
+          )}
+        </div>
 
         <DesktopNav />
 
@@ -31,11 +61,7 @@ const Topbar: FC = () => {
         </IconButton>
       </Toolbar>
 
-      <Drawer
-        anchor="right"
-        open={mobileOpen}
-        onClose={handleDrawerToggle}
-      >
+      <Drawer anchor="right" open={mobileOpen} onClose={handleDrawerToggle}>
         <MobileNav handleDrawerToggle={handleDrawerToggle} />
       </Drawer>
     </AppBar>
