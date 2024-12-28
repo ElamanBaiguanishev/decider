55323b5e99d53784bbe37a482acede10203605f8
ElamanBaiguanishev <elaman1007@mail.ru>
2024-12-28 11:53:20 +0500

    Обновил модель лобби

--- backend/src/lobby/entities/lobby.entity.ts
+++ backend/src/lobby/entities/lobby.entity.ts
@@ -1,20 +1,34 @@
 import { Decider } from 'src/decider/entities/decider.entity';
 import { User } from 'src/users/entities/user.entity';
-import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, ManyToMany, CreateDateColumn, UpdateDateColumn, OneToMany } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+
+export enum LobbyStatus {
+    Created = 'Создана',
+    InProgress = 'В Процессе',
+    Completed = 'Завершена',
+}
+
 
 @Entity('lobbies')
 export class Lobby {
     @PrimaryGeneratedColumn()
     id: number;
 
-    @Column()
-    status: string
-
     @ManyToOne(() => Decider, decider => decider.lobbies)
     decider: Decider
 
     @ManyToOne(() => User, user => user.lobbies)
     creator: User
+
+    @Column()
+    opponent: number
+
+    @Column({
+        type: 'enum',
+        enum: LobbyStatus,
+        default: LobbyStatus.Created,
+    })
+    status: LobbyStatus;
 
     @CreateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
     createdAt: Date;
