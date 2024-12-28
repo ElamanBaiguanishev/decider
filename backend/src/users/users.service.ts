8c1b1b48e07e95cf7889fe3fb25c25626b20d705
ElamanBaiguanishev <elaman1007@mail.ru>
2024-12-28 11:52:51 +0500

    начал разработку модуля юзера

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,19 +1,30 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>
+  ) { }
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async findAll() {
+    return await this.userRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(id: number) {
+    return await this.userRepository.findOne({
+      where: {
+        id
+      }
+    });
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
